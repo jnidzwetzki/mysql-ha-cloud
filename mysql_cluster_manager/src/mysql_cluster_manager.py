b02f07808b15666e8870de655ef9b13771e1712d
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2020-10-26 22:00:53 +0100

    Updated cluster manager

--- mysql_cluster_manager/src/mysql_cluster_manager.py
+++ mysql_cluster_manager/src/mysql_cluster_manager.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
 import time
+import argparse
 
-while True:
-    time.sleep(1)
+parser = argparse.ArgumentParser(description='MySQL cluster manager.')
+
+parser.add_argument('operation', metavar='operation', help='Operation to be executed')
+
+args = parser.parse_args()
+
+if args.operation == 'join_or_bootstrap':
+    while True:
+        time.sleep(1)
+else:
+    print(f"Unknown operation: {args.operation}");
+    exit(1)
+
