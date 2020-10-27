021657661e848abd5220ea0ed62645767c92315a
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2020-10-27 20:18:06 +0100

    Updated client

--- mysql_cluster_manager/src/mysql_cluster_manager.py
+++ mysql_cluster_manager/src/mysql_cluster_manager.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import time
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser(description='MySQL cluster manager.')
 
@@ -11,6 +13,26 @@ parser.add_argument('operation', metavar='operation', help='Operation to be exec
 args = parser.parse_args()
 
 if args.operation == 'join_or_bootstrap':
+
+    consul_args = ["consul"]
+    consul_args.append("agent")
+    consul_args.append("--data-dir")
+    consul_args.append("/tmp/consul")
+
+    consul_interface = os.environ.get("CONSUL_BIND_INTERFACE")
+
+    if consul_interface is not None:
+        consul_args.append("--bind")
+        consul_args.append(f'{{{{ GetInterfaceIP "{consul_interface}" }}}}')
+
+    consul_seed = os.environ.get("CONSUL_BOOTSTRAP_SERVER")
+
+    if consul_seed is not None:
+        consul_args.append("--join")
+        consul_args.append(consul_seed)
+
+    consul_process = subprocess.run(consul_args, capture_output=False, check=True)
+
     while True:
         time.sleep(1)
 else:
