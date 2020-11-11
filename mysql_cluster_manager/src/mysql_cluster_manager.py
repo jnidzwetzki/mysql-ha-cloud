8e7c61ff8849af67fd82fc907fde487891346f91
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2020-11-11 10:53:40 +0100

    Updated parser info

--- mysql_cluster_manager/src/mysql_cluster_manager.py
+++ mysql_cluster_manager/src/mysql_cluster_manager.py
@@ -11,9 +11,14 @@ from mcm.consul import Consul
 from mcm.minio import Minio
 from mcm.mysql import Mysql
 
-parser = argparse.ArgumentParser(description='MySQL cluster manager.')
+parser = argparse.ArgumentParser(
+    description="MySQL cluster manager",
+    epilog="For more info, please see: https://github.com/jnidzwetzki/mysql-ha-cloud")
 
-parser.add_argument('operation', metavar='operation', help='Operation to be executed')
+AVAILABLE_OPERATIONS = "(join_or_bootstrap, mysql_backup, mysql_restore)"
+parser.add_argument('operation', metavar='operation',
+                    help=f'Operation to be executed {AVAILABLE_OPERATIONS}')
+
 log_levels = ('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')
 parser.add_argument('--log-level', default='INFO', choices=log_levels)
 
@@ -40,8 +45,6 @@ logging.basicConfig(level=args.log_level,
 
 if args.operation == 'join_or_bootstrap':
     join_or_bootstrap()
-elif args.operation == 'minio_setup':
-    Minio.setup_connection()
 elif args.operation == 'mysql_backup':
     Mysql.backup_data()
 elif args.operation == 'mysql_restore':
