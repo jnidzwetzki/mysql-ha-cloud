48aa87c0f9faa86fcdd6fa9a2a67c61d96b21654
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2020-11-14 10:56:01 +0100

    Check required envvars

--- mysql_cluster_manager/src/mysql_cluster_manager.py
+++ mysql_cluster_manager/src/mysql_cluster_manager.py
@@ -2,6 +2,7 @@
 
 """This file is part of the MySQL cluster manager"""
 
+import os
 import sys
 import logging
 import argparse
@@ -20,11 +21,25 @@ parser.add_argument('operation', metavar='operation',
 log_levels = ('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')
 parser.add_argument('--log-level', default='INFO', choices=log_levels)
 
+# Parse args
 args = parser.parse_args()
 
+# Configure logging
 logging.basicConfig(level=args.log_level,
                     format='%(levelname)s %(name)s %(message)s')
 
+# Check for all needed env vars
+required_envvars = ['CONSUL_BIND_INTERFACE', 'CONSUL_BOOTSTRAP_SERVER',
+                    'MINIO_ACCESS_KEY', 'MINIO_SECRET_KEY', 'MINIO_URL',
+                    'MYSQL_ROOT_PASSWORD', 'MYSQL_BACKUP_USER', 'MYSQL_BACKUP_PASSWORD',
+                    'MYSQL_REPLICATION_USER', 'MYSQL_REPLICATION_PASSWORD']
+
+for required_var in required_envvars:
+    if not required_var in os.environ:
+        logging.error("Required environment %s not found, exiting", required_var)
+        sys.exit(1)
+
+# Perform operations
 if args.operation == 'join_or_bootstrap':
     Actions.join_or_bootstrap()
 elif args.operation == 'mysql_backup':
@@ -32,5 +47,5 @@ elif args.operation == 'mysql_backup':
 elif args.operation == 'mysql_restore':
     Mysql.restore_data()
 else:
-    print(f"Unknown operation: {args.operation}")
+    logging.error("Unknown operation: %s", {args.operation})
     sys.exit(1)
